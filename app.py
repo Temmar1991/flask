52b05ca8dd21a5de774be8ae853ccbd5e7e183c0
Artem Melnyk <temmar@ukr.net>
2019-04-02 00:12:20 +0300

    Add config settings

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+app.config.from_object(os.environ["APP_SETTINGS"])
 
 
 @app.route('/')
@@ -14,3 +17,4 @@ def hello_name(name):
 
 if __name__ == '__main__':
     app.run()
+    
