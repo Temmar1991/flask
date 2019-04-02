dcedcd3770e296546c3a9444d78c58782e5523a8
Artem Melnyk <temmar@ukr.net>
2019-04-03 01:30:50 +0300

    Database setup for postgres

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ["APP_SETTINGS"])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+from models import Result
 
 
 @app.route('/')
@@ -17,4 +23,4 @@ def hello_name(name):
 
 if __name__ == '__main__':
     app.run()
-    
+
